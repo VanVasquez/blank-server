f15ad93df673045024f167748c6eff662e790f49
van <van973vv@gmail.com>
2023-04-30 09:37:34 +0800

    allow

--- src/allowedOrigin.js
+++ src/allowedOrigin.js
@@ -1,3 +1,3 @@
 //Only add the link of websites that you want to have access with
 
-module.exports = ["http://localhost:3000"];
+module.exports = ['http://localhost:3000', 'https://blank-client.vercel.app'];
